b5e0e13e5b6e186de8a6e4d56b1ede8a93a94e9c
TAY TS <taytzushieh@gmail.com>
2020-06-27 22:26:53 +0800

    Done Group.h implementation

--- WEEK6/Assignment5_taytzushieh_1002946/src/Group.h
+++ WEEK6/Assignment5_taytzushieh_1002946/src/Group.h
@@ -1,50 +1,57 @@
 #ifndef GROUP_H
 #define GROUP_H
 
-
 #include "Object3D.h"
 #include "Ray.h"
 #include "Hit.h"
 #include <iostream>
 
-using  namespace std;
-
+using namespace std;
 
 /// TODO: Implement Group
-class Group:public Object3D
-{
-public:
-
-  Group(){
-
-  }
-	
-  Group( int num_objects ){
-     
+class Group : public Object3D {
+ public:
+  Group() {
   }
 
-  ~Group(){
-   
+  Group(int num_objects) {
+    // Set space for storing the Object3D
+    objects.resize(num_objects);
+  }
+
+  ~Group() {
   }
 
   // TODO
-  virtual bool intersect( const Ray& r , Hit& h , float tmin ) {
-    
-   }
-	
-  // TODO
-  void addObject( int index , Object3D* obj ){
+  virtual bool intersect(const Ray& r, Hit& h, float tmin) {
+    // check if the ray intersect with one of the object
+    bool check = false;
+    for (Object3D* obj : objects) {
+      if (obj != nullptr) {
+        check |= obj->intersect(r, h, tmin);
+        if (check) {
+          break;
+        }
+      }
+    }
 
+    return check;
   }
 
-  int getGroupSize(){ 
-      return (int)objects.size();
+  // TODO
+  void addObject(int index, Object3D* obj) {
+    // Add the Object3D pointer if the index within the range of the storage vector
+    if (index >= 0 && index < objects.size()) {
+      objects.at(index) = obj;
+    }
+  }
+
+  int getGroupSize() {
+    return (int)objects.size();
   }
 
  private:
- 
-    vector<Object3D*> objects;
+  vector<Object3D*> objects;
 };
 
 #endif
-	

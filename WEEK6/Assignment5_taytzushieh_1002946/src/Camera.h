169edc3d845a5a414765338f2d627a13f0d5f37d
TAY TS <taytzushieh@gmail.com>
2020-06-30 01:05:24 +0800

    Done implement the Camera.h

--- WEEK6/Assignment5_taytzushieh_1002946/src/Camera.h
+++ WEEK6/Assignment5_taytzushieh_1002946/src/Camera.h
@@ -6,47 +6,60 @@
 #include <float.h>
 #include <cmath>
 
+class Camera {
+ public:
+  //generate rays for each screen-space coordinate
+  virtual Ray generateRay(const Vector2f& point) = 0;
 
-class Camera
-{
-public:
-	//generate rays for each screen-space coordinate
-	virtual Ray generateRay( const Vector2f& point ) = 0 ; 
-	
-	virtual float getTMin() const = 0 ; 
-	virtual ~Camera(){}
-protected:
-	Vector3f center; 
-	Vector3f direction;
-	Vector3f up;
-	Vector3f horizontal;
+  virtual float getTMin() const = 0;
+  virtual ~Camera() {}
 
+ protected:
+  Vector3f center;
+  Vector3f direction;
+  Vector3f up;
+  Vector3f horizontal;
 };
 
 /// TODO: Implement Perspective camera
-class PerspectiveCamera: public Camera
-{
-public:
+class PerspectiveCamera : public Camera {
+ public:
+  // TODO
+  PerspectiveCamera(const Vector3f& center, const Vector3f& direction, const Vector3f& up, float angle) {
+    w = Vector3f(direction).normalized();     // make a copy of the camera direction
+    u = Vector3f::cross(w, up).normalized();  // camera right
+    v = Vector3f::cross(u, w).normalized();   // camera up
 
-    // TODO
-	PerspectiveCamera(const Vector3f& center, const Vector3f& direction,const Vector3f& up , float angle){
+    this->angle = angle;
+    this->center = center;
 
-	}
+    // transformation matrix from standard camera to extrinsic camera
+    // pre-compute to speed up the generateRay process
+    this->viewTransMat = Matrix4f(
+        Vector4f(this->u, 0.0f),
+        Vector4f(this->v, 0.0f),
+        -Vector4f(this->w, 0.0f),
+        Vector4f(this->center, 1.0f));
+  }
 
-    // TODO
-	virtual Ray generateRay( const Vector2f& point){
-    
-    }
+  // TODO
+  virtual Ray generateRay(const Vector2f& point) {
+    float fieldOfViewAngle = tan(this->angle / 2.0f);
+    Vector4f rayDirection = Vector4f(point.x() * fieldOfViewAngle, point.y() * fieldOfViewAngle, -1.0f, 0.0f);
+    Vector3f transRayDir = (this->viewTransMat * rayDirection).xyz().normalized();
+    return Ray(this->center, transRayDir);
+  }
 
-	virtual float getTMin() const { 
-		return 0.0f;
-	}
+  virtual float getTMin() const {
+    return 0.0f;
+  }
 
-private:
-    float angle;
-    Vector3f w;
-    Vector3f u;
-    Vector3f v;
+ private:
+  float angle;
+  Vector3f w;
+  Vector3f u;
+  Vector3f v;
+  Matrix4f viewTransMat;
 };
 
-#endif //CAMERA_H
+#endif  //CAMERA_H

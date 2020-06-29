eadc6a0b1fe45ed773f80f949aa15e6f95a34604
TAY TS <taytzushieh@gmail.com>
2020-06-30 01:05:37 +0800

    Done implement Plane.h

--- WEEK6/Assignment5_taytzushieh_1002946/src/Plane.h
+++ WEEK6/Assignment5_taytzushieh_1002946/src/Plane.h
@@ -6,31 +6,34 @@
 #include <cmath>
 using namespace std;
 
-
 /// TODO: Implement Plane representing an infinite plane
-class Plane: public Object3D
-{
-public:
-	Plane(){}
+class Plane : public Object3D {
+ public:
+  Plane() {}
 
-	Plane( const Vector3f& normal , float d , Material* m):Object3D(m){
-        this->normal = normal.normalized();
-        this->d = -d;
-	}
+  Plane(const Vector3f& normal, float d, Material* m) : Object3D(m) {
+    this->normal = normal.normalized();
+    this->d = -d;
+  }
 
-	~Plane(){}
+  ~Plane() {}
 
+  // TODO
+  virtual bool intersect(const Ray& r, Hit& h, float tmin) {
+    const float normalDotRayOrigin = Vector3f::dot(this->normal, r.getOrigin());
+    const float normalDotRayDirection = Vector3f::dot(this->normal, r.getDirection());
+    const float t = (-this->d - normalDotRayOrigin) / normalDotRayDirection;
 
-    // TODO
-	virtual bool intersect( const Ray& r , Hit& h , float tmin){
-        
-	}
+    if (t > tmin && t < h.getT()) {
+      h.set(t, this->material, this->normal);
+      return true;
+    }
 
-protected:
-    Vector3f normal;
-    float d;
-    
+    return false;
+  }
+
+ protected:
+  Vector3f normal;
+  float d;
 };
-#endif //PLANE_H
-		
-
+#endif  //PLANE_H
